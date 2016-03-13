9e0a97f3563c89a83f5e8a6e20b171ae4a592134
Anthony Patterson <anthony.patterson2.0@gmail.com>
2016-03-13 13:20:25 +0000

    add activities, dataset names

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,7 @@
-library(tidyr)##starting from the point where we have downloaded and unzipped the dataset
+library(tidyr)
+library(dplyr)
+
+##starting from the point where we have downloaded and unzipped the dataset
 
 ##examine the folders and subfolders
 root <- "UCI HAR Dataset"
@@ -11,15 +14,42 @@ features <- read.table("UCI HAR Dataset/features.txt")
 ##read the test data
 testdata <- read.table("UCI HAR Dataset/test/X_test.txt")
 
+
+testlabels <- read.table("UCI HAR Dataset/test/Y_test.txt")
+
 ##apply the feature names to the data
 names(testdata) <- features[,2]
-testdata$dataset = "test"
+
+##keep only the columns with mean or std in title
+testdata <- testdata[,grep("[Mm]ean|std", names(testdata))]
+
+testdata$dataset <- "test"
+
+##apply activity names
+names(testlabels) <- c("number")
+testdata <- cbind(testdata, testlabels)
 
 ##read the training data
 traindata <- read.table("UCI HAR Dataset/train/X_train.txt")
-names(traindata) <- features[,2]
-traindata$dataset = "train"
 
+trainlabels <- read.table("UCI HAR Dataset/train/Y_train.txt")
+names(traindata) <- features[,2]
+
+##keep only the columns with mean or std in title
+traindata <- traindata[,grep("[Mm]ean|std", names(traindata))]
+traindata$dataset <- "train"
+
+names(trainlabels) <- c("number")
+traindata <- cbind(traindata, trainlabels)
+
+##merge the two sets
 merged <- rbind(testdata, traindata)
 
-print(head(merged))
+##now add the activity labels
+labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+names(labels) <- c("number", "activity")
+merged <- merge(labels, merged)
+merged <- select(merged, -number)
+
+##finally write it out to a file for future use
+write.table(merged, "UCI_tidy.csv")

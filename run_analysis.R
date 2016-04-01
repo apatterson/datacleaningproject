6738f77b64619f0520b426df9cbf79dde77432e0
Anthony Patterson <anthony.patterson2.0@gmail.com>
2016-04-01 22:30:41 +0100

    updates following review

--- run_analysis.R
+++ run_analysis.R
@@ -28,6 +28,9 @@ testdata$dataset <- "test"
 ##apply activity names
 names(testlabels) <- c("number")
 testdata <- cbind(testdata, testlabels)
+testsubject <- read.table("UCI HAR Dataset/test/subject_test.txt")
+names(testsubject) <- c("subject")
+testdata <- cbind(testdata, testsubject)
 
 ##read the training data
 traindata <- read.table("UCI HAR Dataset/train/X_train.txt")
@@ -41,6 +44,9 @@ traindata$dataset <- "train"
 
 names(trainlabels) <- c("number")
 traindata <- cbind(traindata, trainlabels)
+trainsubject <- read.table("UCI HAR Dataset/train/subject_train.txt")
+names(trainsubject) <- c("subject")
+traindata <- cbind(traindata, trainsubject)
 
 ##merge the two sets
 merged <- rbind(testdata, traindata)
@@ -52,4 +58,10 @@ merged <- merge(labels, merged)
 merged <- select(merged, -number)
 
 ##finally write it out to a file for future use
-write.table(merged, "UCI_tidy.csv")
+write.table(merged, "UCI_tidy.txt",row.name=FALSE)
+
+##create an extra file with averages
+avgs <- select(aggregate(merged, list(Activity=merged$activity,
+                                      Subject=merged$subject), mean),
+               -activity, -dataset)
+write.table(avgs, "UCI_avg.txt", row.name=FALSE)
